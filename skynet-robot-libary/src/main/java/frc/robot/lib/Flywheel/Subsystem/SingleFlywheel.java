cfef953bec1b0fcd10394e2e3cedc9a601d5a984
yotam <yotam201410@gmail.com>
2022-07-24 17:24:45 +0300

    base is done now need to make subsystem

---
+++ skynet-robot-libary/src/main/java/frc/robot/lib/Flywheel/Subsystem/SingleFlywheel.java
@@ -0,0 +1,34 @@
+package frc.robot.lib.Flywheel.Subsystem;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.system.plant.DCMotor;
+import frc.robot.lib.base.MotorControllers.MotorControllerBase;
+import frc.robot.lib.base.Subsytems.MySubsytemBase;
+import frc.robot.lib.Physics.Constants;
+import frc.robot.lib.Physics.ShootingPhysics;
+
+public class SingleFlywheel extends MySubsytemBase{
+    protected ArrayList<Pair<MotorControllerBase,DCMotor>> left_side;
+    protected ArrayList<Pair<MotorControllerBase,DCMotor>> right_side;
+    protected double wheel_radius_meters;
+
+
+
+    public SingleFlywheel(ArrayList<Pair<MotorControllerBase,DCMotor>> left_side, ArrayList<Pair<MotorControllerBase,DCMotor>> right_side,double wheel_radius_meters)
+    {
+        this.left_side = left_side;
+        this.right_side = right_side;
+        this.wheel_radius_meters = wheel_radius_meters;
+    }
+    public double get_starting_velocity_for_distance_and_angle_without_friction(double angle, double direct_distance)
+    {
+        return 60 * ShootingPhysics.get_starting_velocity_for_distance_and_angle(angle, direct_distance) / (2 * Math.PI*wheel_radius_meters);
+    }
+    
+
+
+    
+
+}
---
+++ skynet-robot-libary/src/main/java/frc/robot/lib/Physics/Constants.java
@@ -0,0 +1,5 @@
+package frc.robot.lib.Physics;
+
+public final class Constants {
+    public static final double gravitational_acceleration = 9.807; // m/s^2
+}
